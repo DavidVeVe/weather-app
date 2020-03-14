98ea3ecefc879166770e46429b743b978c004532
Armando Velazquez <davidarmando.gdl@live.com>
2020-03-13 20:04:14 -0600

    Updates

---
+++ precache-manifest.b0d537e3f11ae0910e991d1e42407415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e8db3e1db0f2e2cb1fd661e00e0825",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ba8696f9034062aadf42",
+    "url": "/weather-app/static/css/main.ccf2d674.chunk.css"
+  },
+  {
+    "revision": "284a42eb5126757585a1",
+    "url": "/weather-app/static/js/2.57054c52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.57054c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8696f9034062aadf42",
+    "url": "/weather-app/static/js/main.8f261817.chunk.js"
+  },
+  {
+    "revision": "0a2842769c56eec09ba5",
+    "url": "/weather-app/static/js/runtime-main.40251a1f.js"
+  }
+]);
